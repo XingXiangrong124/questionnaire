d37a7a834e65232b5862ab37dc1d7e0edaadc08c
XingXiangrong124 <1241728503@qq.com>
2023-08-09 21:24:13 +0800

    add:回收站单多选恢复/删除

--- src/components/RubbishTable.tsx
+++ src/components/RubbishTable.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
-import { Table, Tag, Space } from 'antd';
+import { FC, useState } from 'react';
+import { Table, Tag, Space, Button, Modal, message } from 'antd';
 import Column from 'antd/es/table/Column';
+import { ExclamationCircleTwoTone } from '@ant-design/icons';
 const columnsTable = [
   {
     title: '问卷名称',
@@ -24,21 +25,21 @@ const columnsTable = [
 ];
 const data = [
   {
-    key: '1',
+    _id: 'q1',
     name: '问卷test1',
     isPublished: true,
     answerCount: 11,
     createAt: '8月1日 20:30',
   },
   {
-    key: '2',
+    _id: 'q2',
     name: '问卷test2',
     isPublished: false,
     answerCount: 110,
     createAt: '8月12日 20:30',
   },
   {
-    key: '3',
+    _id: 'q3',
     name: '问卷test3',
     isPublished: true,
     answerCount: 54,
@@ -46,29 +47,92 @@ const data = [
   },
 ];
 const RubbishTable: FC = () => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[]) => {
+      setSelectedItems(selectedRowKeys as string[]);
+    },
+  };
+  const showWarn = () => {
+    setDeleteOpen(true);
+  };
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setDeleteOpen(false);
+      setLoading(false);
+      messageApi.open({
+        type: 'success',
+        content: '彻底删除成功',
+      });
+    }, 1000);
+  };
+  const handleCancel = () => {
+    setDeleteOpen(false);
+  };
   return (
-    <Table dataSource={data}>
-      {columnsTable.map(item => {
-        const { title, dataIndex, render } = item;
-        return (
-          <Column
-            title={title}
-            dataIndex={dataIndex}
-            render={value => (render ? render(value) : value)}
-          ></Column>
-        );
-      })}
-      <Column
-        title="操作"
-        key="action"
-        render={(_: any, record) => (
-          <Space size="middle">
-            <a style={{ fontSize: '13px' }}>恢复</a>
-            <a style={{ fontSize: '13px' }}>删除</a>
+    <>
+      {contextHolder}
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+        <Space>
+          <Button type="primary" disabled={selectedItems.length === 0}>
+            批量恢复
+          </Button>
+          <Button danger disabled={selectedItems.length === 0} onClick={showWarn}>
+            批量删除
+          </Button>
+        </Space>
+      </div>
+      <Table
+        dataSource={data}
+        rowKey={q => q._id}
+        rowSelection={{
+          type: 'checkbox',
+          ...rowSelection,
+        }}
+      >
+        {columnsTable.map(item => {
+          const { title, dataIndex, render } = item;
+          return (
+            <Column
+              title={title}
+              dataIndex={dataIndex}
+              render={value => (render ? render(value) : value)}
+              key={dataIndex}
+            ></Column>
+          );
+        })}
+        <Column
+          title="操作"
+          key="action"
+          render={(_: any, record) => (
+            <div style={{ marginLeft: '-45px' }}>
+              <Button type="link" style={{ fontSize: '14px' }}>
+                恢复
+              </Button>
+              <Button type="link" style={{ fontSize: '14px' }} onClick={showWarn}>
+                删除
+              </Button>
+            </div>
+          )}
+        />
+      </Table>
+      <Modal
+        title={
+          <Space>
+            <ExclamationCircleTwoTone twoToneColor="#FFD700" style={{ fontSize: '20px' }} />
+            <span>确定要彻底删除此问卷吗？</span>
           </Space>
-        )}
-      />
-    </Table>
+        }
+        open={deleteOpen}
+        onOk={handleOk}
+        confirmLoading={loading}
+        onCancel={handleCancel}
+      ></Modal>
+    </>
   );
 };
 export default RubbishTable;
