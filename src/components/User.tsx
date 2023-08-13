a598235232a712aaa1b74179ca6bf38c356fc9b8
XingXiangrong124 <1241728503@qq.com>
2023-08-13 13:14:43 +0800

    add: 用户信息&用户退出

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,11 +1,38 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Space, message } from 'antd';
+import { useRequest } from 'ahooks';
+import { UserSwitchOutlined } from '@ant-design/icons';
+import { getUserInfo } from '../api/user';
 import { LOGIN_PATHNAME } from '../router';
+import { removeToken } from '../utils/token/user-token';
 const User: FC = () => {
-  return (
-    <>
-      <Link to={LOGIN_PATHNAME}>登录</Link>
-    </>
-  );
+  const nav = useNavigate();
+  const { data } = useRequest(getUserInfo);
+  const { username } = (data as any) || '';
+  const logout = () => {
+    removeToken();
+    message.success('退出成功');
+    nav(LOGIN_PATHNAME);
+  };
+  const UserInfo = () => {
+    return (
+      <>
+        <div>
+          <Space style={{ color: 'white', fontWeight: 'bold' }}>
+            <UserSwitchOutlined />
+            <span>{username}</span>
+          </Space>
+          <Button type="link" onClick={logout}>
+            登出
+          </Button>
+        </div>
+      </>
+    );
+  };
+  const Login = () => {
+    return <Link to={LOGIN_PATHNAME}>登录</Link>;
+  };
+  return <>{username ? <UserInfo /> : <Login />}</>;
 };
 export default User;
