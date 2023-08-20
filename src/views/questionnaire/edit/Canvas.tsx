4597181880227ed6111607d9b75c360686b2dbb5
XingXiangrong124 <1241728503@qq.com>
2023-08-21 07:59:36 +0800

    add: 读取ajax列表信息显示在页面上

--- src/views/questionnaire/edit/Canvas.tsx
+++ src/views/questionnaire/edit/Canvas.tsx
@@ -1,12 +1,23 @@
 import { FC } from 'react';
 import { Spin } from 'antd';
-import TitleComponent from '../../../components/Question/title/TitleComponent';
-import InputComponent from '../../../components/Question/Input/InputComponent';
+// import TitleComponent from '../../../components/Question/title/TitleComponent';
+// import InputComponent from '../../../components/Question/Input/InputComponent';
 import styles from './Canvas.module.scss';
+import useGetComponents from '../../../hooks/useGetComponents';
+import { getComponentByType } from '../../../components/Question';
+import { ComponentInfoType } from '../../../store/questionReducer/componentReducer';
 type PropsType = {
   loading: boolean;
 };
+function getComponent(componentInfo: ComponentInfoType) {
+  const { type, props } = componentInfo;
+  const ComponentConfig = getComponentByType(type);
+  if (!ComponentConfig) return null;
+  const { Component } = ComponentConfig;
+  return <Component {...props} />;
+}
 const Canvas: FC<PropsType> = ({ loading }) => {
+  const componentList = useGetComponents();
   if (loading) {
     return (
       <div style={{ textAlign: 'center', marginTop: '25vh' }}>
@@ -16,7 +27,15 @@ const Canvas: FC<PropsType> = ({ loading }) => {
   }
   return (
     <div className={styles.canvas}>
-      <div className={styles['content-wrapper']}>
+      {componentList.map(c => {
+        const { fe_id } = c;
+        return (
+          <div key={fe_id} className={styles['content-wrapper']}>
+            <div className={styles.control}>{getComponent(c)}</div>
+          </div>
+        );
+      })}
+      {/* <div className={styles['content-wrapper']}>
         <div className={styles.control}>
           <TitleComponent></TitleComponent>
         </div>
@@ -25,8 +44,7 @@ const Canvas: FC<PropsType> = ({ loading }) => {
         <div className={styles.control}>
           <InputComponent></InputComponent>
         </div>
-      </div>
-      <div></div>
+      </div> */}
     </div>
   );
 };
